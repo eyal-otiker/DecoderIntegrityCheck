4f38aa47a2a4f2600f2f36adfd3f43d29e7c4660
eyal <eyal13102@gmail.com>
2025-11-23 19:25:13 +0200

    [ADD] Converter

---
+++ DecoderLibrary/ConverterClass/JsonToIcdItemConverter.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace DecoderLibrary
+{
+    public class JsonToIcdItemConverter<IcdDataType, GetParametersType> where GetParametersType : IIcdItemParameters<IcdDataType>
+    {
+        private string _icdText;
+        private GetParametersType _itemGetParameters;
+
+        public JsonToIcdItemConverter(string icdText, GetParametersType convertsItem)
+        {
+            this._icdText = icdText;
+            this._itemGetParameters = convertsItem;
+        }
+
+        public Dictionary<string, IcdDataType> ConverterManager()
+        {
+            List<IcdDataType> icdItemsList = ConvertJsonToIcdItemList();
+            Dictionary<string, IcdDataType> icdItemsDictioanry = ConvertListIcdItemsToDictionary(icdItemsList);
+            return icdItemsDictioanry;
+        }
+
+        /// <summary>
+        /// function that do deserealize to the text of the ICD file
+        /// </summary>
+        /// <param name="icdText"></param>
+        /// <returns></returns>
+        private List<IcdDataType> ConvertJsonToIcdItemList()
+        {
+            List<IcdDataType> listItems = JsonConvert.DeserializeObject<List<IcdDataType>>(this._icdText);
+            return listItems;
+        }
+
+        /// <summary>
+        /// convert the list of type T (the type of the ICD item) to dictionary of type U (the encoded of T type)
+        /// </summary>
+        /// <param name="listOriginalItems"></param>
+        /// <returns></returns>
+        private Dictionary<string, IcdDataType> ConvertListIcdItemsToDictionary(List<IcdDataType> list)
+        {
+            Dictionary<string, IcdDataType> dictionaryIcdItems = new Dictionary<string, IcdDataType>();
+
+            foreach (IcdDataType originalItem in list)
+            {
+                string itemName = this._itemGetParameters.NameOfItem(originalItem);
+                if (!dictionaryIcdItems.ContainsKey(itemName))
+                    dictionaryIcdItems.Add(itemName, originalItem);
+            }
+                              
+            return dictionaryIcdItems;
+        }
+    }
+}
---
+++ DecoderLibrary/ConvertingClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DecoderLibrary
+{
+    internal static class ConvertingClass
+    {
+        public static int ConvertCorrelateToNumber(string corrString)
+        {
+            string convertCorr = string.Empty;
+            char[] corrCharArray = corrString.ToCharArray();
+            
+            for (int i = 1; i < corrCharArray.Length - 1; i++)
+            {
+                if (corrCharArray[i] != '0')
+                    convertCorr += corrCharArray[i];
+            }
+
+            if (convertCorr != string.Empty)
+                return int.Parse(convertCorr);
+            else
+                return 0;
+        }
+        
+        public static int ConvertByteToNumber(List<byte> stringValue, bool canValueBeNegative = false)
+        {
+            int num = 0; int mult = 1; int firstIndexValue = -1;
+   
+            for (int i = stringValue.Count - 1; i >= 0; i--)
+            {
+                if (stringValue[i] == 0 || stringValue[i] == 1)
+                {
+                    firstIndexValue = i;
+                    num += stringValue[i] * mult;
+                    mult *= 2;
+                }           
+            }
+
+            if (canValueBeNegative && firstIndexValue != -1 && stringValue[firstIndexValue] == '1')
+                num -= (int)Math.Pow(2, stringValue.Count);
+
+            return num;
+        }
+
+        public static int ConvertByteToNumber(string stringValue)
+        {
+            int num = 0; int mult = 1;
+
+            for (int i = stringValue.Length - 1; i >= 0; i--)
+            {
+                if (stringValue[i] == '0' || stringValue[i] == '1')
+                {
+                    num += (stringValue[i] - '0') * mult;
+                    mult *= 2;
+                }
+            }
+
+            return num;
+        }
+
+        public static List<byte> ConvertNumberToByte(int num, int maskLength)
+        {
+            List<byte> byteNumber = new List<byte>();
+            int count = 0;
+
+            if (num < 0)
+                num = (int)Math.Pow(2, maskLength) - Math.Abs(num);
+
+            while (num != 0 || count < maskLength)
+            {
+                byteNumber.Add((byte)((byte)num % 2));
+                num /= 2;
+                count++;
+            }
+
+            return byteNumber;
+        }
+    }
+}
